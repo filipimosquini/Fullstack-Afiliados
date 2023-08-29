8650898a542054a8ec213d684cf22bbdfb16794d
Filipi Mosquini <filipimosquini@gmail.com>
2023-08-29 00:26:17 -0300

    [Frontend] Cria os components footer, not found e logged user

--- src/Frontend.Web/src/app/navigate/logged-user/logged-user.component.ts
+++ src/Frontend.Web/src/app/navigate/logged-user/logged-user.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LocalStorageUtils } from 'src/app/utils/localstorage';
 
 @Component({
   selector: 'app-logged-user',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./logged-user.component.css']
 })
 export class LoggedUserComponent {
+  token: string = "";
+  user: any;
+  email: string = "";
+  localStorageUtils = new LocalStorageUtils();
 
+  constructor(private router: Router) {  }
+
+  loggedUser(): boolean {
+    this.token = this.localStorageUtils.getUserToken();
+    this.user = this.localStorageUtils.getUser();
+
+    if (this.user)
+      this.email = this.user.email;
+
+    return this.token !== null;
+  }
+
+  logout() {
+    this.localStorageUtils.cleanUserData();
+    this.router.navigate(['/users/sign-in']);
+  }
 }
